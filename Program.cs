1c95d7a8935c354204217bd25355b4a5ac72e260
Sanier <Sanier20@mail.ru>
2021-10-04 19:15:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _7._3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processor proc = new Processor();
+            MotherBoard mot = new MotherBoard();
+            GraphicCard gc = new GraphicCard();
+
+
+        }
+
+        abstract class ComputerPart
+        {
+            public abstract void Work();
+        }
+
+        class Processor : ComputerPart
+        {
+            public override void Work()
+            {
+                Console.WriteLine("Processor");
+            }
+        }
+
+        class MotherBoard : ComputerPart
+        {
+            public override void Work()
+            {
+                Console.WriteLine("MotherBoard");
+            }
+        }
+
+        class GraphicCard : ComputerPart
+        {
+            public override void Work()
+            {
+                Console.WriteLine("GraphicCard");
+            }
+        }
+    }
+}
